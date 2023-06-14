c393569767cbb35e39c34e0c97c72ab38d01e901
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-06-14 11:47:48 +0300

    Lisätty lukujen tulostus numerojärjestyksessä.

--- Python/LuvutJaFrekvenssit.py
+++ Python/LuvutJaFrekvenssit.py
@@ -1,7 +1,14 @@
+luvut = []
 
 while True:
     syöte = input("Syötä luku 1-99 tai nolla lopettaaksesi: ")
     if syöte == "0":
         break
+    luvut.append(int(syöte))
 
-print("Loppu.")
+print("Luvut syötetty.")
+luvut.sort()
+
+print("Luvut numerojärjestyksessä:")
+for luku in luvut:
+    print(luku)
