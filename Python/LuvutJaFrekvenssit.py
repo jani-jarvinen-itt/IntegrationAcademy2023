eaab83b2088cc3bc27fefe2a19c4f74ad61e86b0
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-06-14 13:14:13 +0300

    Lisätty lukujen frekvenssien laskentaa.

--- Python/LuvutJaFrekvenssit.py
+++ Python/LuvutJaFrekvenssit.py
@@ -1,4 +1,5 @@
 luvut = []
+frekvenssit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 while True:
     syöte = input("Syötä luku 1-99 tai nolla lopettaaksesi: ")
@@ -6,9 +7,18 @@ while True:
         break
     luvut.append(int(syöte))
 
+    lukualue = int(syöte) // 10
+    frekvenssit[lukualue] += 1
+
 print("Luvut syötetty.")
 luvut.sort()
 
 print("Luvut numerojärjestyksessä:")
 for luku in luvut:
     print(luku)
+
+print("Frekvenssit:")
+for määrä in frekvenssit:
+    print(määrä)
+
+print("Loppu.")
