d0ed161bcf3f72a918949b78e40985664b515897
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-09 15:24:08 +0300

    Lisätty Python-esimerkki SQLite-tietokantaan tallentamisesta. Lisäksi HTTP-API:n kutsu.

---
+++ Python/HttpLukuJaSQLiteKirjoitus.py
@@ -0,0 +1,32 @@
+import http.client
+import json
+import sqlite3
+
+# vaihe 1, JSONPlaceholder-palvelun API:n kutsuminen
+host = "jsonplaceholder.typicode.com"
+conn = http.client.HTTPSConnection(host)
+conn.request("GET", "/users", headers={"Host": host})
+response = conn.getresponse()
+
+jsondata = response.read().decode()
+käyttäjät = json.loads(jsondata)
+
+# SQLite-tietokannan käsittely
+print("Aloitetaan SQLite-tietokannan käsittely.")
+yhteys = sqlite3.connect("käyttäjät.db")
+print("Tietokantayhteys avattu.")
+kursori = yhteys.cursor()
+
+# vaihe 2, tallennus SQLite-tietokantaan
+for käyttäjä in käyttäjät:
+    nimi = käyttäjä["name"]
+    kaupunki = käyttäjä["address"]["city"]
+    puhelin = käyttäjä["phone"]
+    
+    kursori.execute("INSERT INTO käyttäjät VALUES ('"+nimi+"', '"+kaupunki+"', '"+puhelin+"')")
+    print("Lisätty käyttäjä:", nimi)
+
+print("Tallennetaan muutokset tietokantaan.")
+yhteys.commit()
+
+print("Suoritus päättyy.")
