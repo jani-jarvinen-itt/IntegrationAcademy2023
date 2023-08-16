6a30f86ccf0361c0fd8c6287fa2095e764cb4c29
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-16 15:23:41 +0300

    Aloitettu koodamaan CSV-tiedoston lukua C#:lla.

--- DotNet/WebApiTesti/Controllers/OmaTestiController.cs
+++ DotNet/WebApiTesti/Controllers/OmaTestiController.cs
@@ -1,13 +1,30 @@
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiTesti.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("kokeilu")]
 public class OmaTestiController : ControllerBase
 {
+    [Route("moi")]
     public string SanoMoi()
     {
         return "Moi maailma!";
+    }
+
+    [Route("aika")]
+    public DateTime Kellonaika()
+    {
+        return DateTime.Now;
+    }
+
+    [Route("csvluku")]
+    public string LueCsvTiedosto()
+    {
+        string tiedostonimi = @"C:\Integration\Koodit\DotNet\WebApiTesti\Henkilöt.csv";
+        string csv = System.IO.File.ReadAllText(tiedostonimi);
+
+        return csv;
     }
 }
