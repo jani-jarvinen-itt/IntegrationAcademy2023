25285a72bdb213ec63b2c6175a7f1230da172c44
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-16 10:44:39 +0300

    Lisätty C#-koodiin null-käsittely.

--- DotNet/Laskin/Program.cs
+++ DotNet/Laskin/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Syötä ensimmäinen luku:");
-string luku1 = Console.ReadLine();
+string luku1 = Console.ReadLine() ?? "0";
 
 Console.WriteLine("Syötä toinen luku:");
-string luku2 = Console.ReadLine();
+string luku2 = Console.ReadLine() ?? "0";
 
 int summa = int.Parse(luku1) + int.Parse(luku2);
 Console.WriteLine("Summa on: " + summa);
