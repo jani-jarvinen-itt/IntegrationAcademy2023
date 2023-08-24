27e8eae19810e919b7c7ba093f91720760329627
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-24 13:14:43 +0300

    Aloitettu JSON-tulostuksen toteutus käyttäen malli- (model) luokkaa.

--- DotNet/PalkanlaskentaDataMuunnos/PalkanlaskentaDataMuunnos/Program.cs
+++ DotNet/PalkanlaskentaDataMuunnos/PalkanlaskentaDataMuunnos/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml;
+﻿using PalkanlaskentaDataMuunnos;
+using System.Xml;
 
 XmlDocument palkkatiedot = new();
 palkkatiedot.Load(@"..\..\..\Palkanlaskenta.xml");
 Console.WriteLine("XML-tiedosto ladattu.");
 
 XmlNode juuri = palkkatiedot.DocumentElement;
+List<PalkkaTietomalli> tulokset = new();
 
 for (int i = 0; i < juuri.ChildNodes.Count; i++)
 {
@@ -13,6 +15,21 @@ for (int i = 0; i < juuri.ChildNodes.Count; i++)
     string nimi = juuri.ChildNodes[i].ChildNodes[1].InnerText;
 
     Console.WriteLine($"Nimi: {nimi}, palkka: {palkka}");
+
+    tulokset.Add(new PalkkaTietomalli()
+    {
+        personName = nimi,
+        salary = new Salary()
+        {
+            monthly = float.Parse(palkka)
+        }
+    });
 }
+
+Console.WriteLine("----------------");
+string json = System.Text.Json.JsonSerializer.Serialize(tulokset);
+Console.WriteLine(json);
+Console.WriteLine("----------------");
+
 
 Console.WriteLine("Loppu.");
