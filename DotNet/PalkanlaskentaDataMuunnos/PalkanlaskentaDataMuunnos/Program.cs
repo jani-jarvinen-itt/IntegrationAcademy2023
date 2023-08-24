72da043d46bacb79478b85774b2a5e4129a0ebfc
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-24 11:47:06 +0300

    Aloitettu koodamaan C#:lla palkka-aineiston lukua. Lisätty esimerkkitiedostot osaksi projekti.

--- DotNet/PalkanlaskentaDataMuunnos/PalkanlaskentaDataMuunnos/Program.cs
+++ DotNet/PalkanlaskentaDataMuunnos/PalkanlaskentaDataMuunnos/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+
+XmlDocument palkkatiedot = new();
+palkkatiedot.Load(@"..\..\..\Palkanlaskenta.xml");
+Console.WriteLine("XML-tiedosto ladattu.");
+
+XmlNode juuri = palkkatiedot.DocumentElement;
+
+for (int i = 0; i < juuri.ChildNodes.Count; i++)
+{
+    // Console.WriteLine(juuri.ChildNodes[i].InnerText);
+    string palkka = juuri.ChildNodes[i].ChildNodes[0].InnerText;
+    string nimi = juuri.ChildNodes[i].ChildNodes[1].InnerText;
+
+    Console.WriteLine($"Nimi: {nimi}, palkka: {palkka}");
+}
+
+Console.WriteLine("Loppu.");
