de7ac5c9dbd1a8e6e1db239be4e0652208378234
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-24 13:06:29 +0300

    Lisätty esimerkki JSON-tavoiterakenteesta sekä tätä rakennetta vastaavat C#-luokat.

---
+++ DotNet/PalkanlaskentaDataMuunnos/PalkanlaskentaDataMuunnos/PalkkaTietomalli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalkanlaskentaDataMuunnos
+{
+    public class PalkkaTietomalli
+    {
+        public string personName { get; set; }
+        public Salary salary { get; set; }
+        public string hireDate { get; set; }
+    }
+
+    public class Salary
+    {
+        public float monthly { get; set; }
+    }
+}
