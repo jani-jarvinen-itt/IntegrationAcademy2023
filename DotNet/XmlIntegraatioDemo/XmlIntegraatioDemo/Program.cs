d1070958eefcc1174464a4b4278bdf2e804a4889
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-25 13:14:09 +0300

    Aloitettu C#-integraatiodemon koodaus. Input-kansion käsittely on alustavasti paikallaan.

---
+++ DotNet/XmlIntegraatioDemo/XmlIntegraatioDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿const string SisäänlukuKansio = @"C:\Integration\Demo\Input";
+const string KäsitellytKansio = @"C:\Integration\Demo\Processed";
+
+string[] tiedostot = Directory.GetFiles(SisäänlukuKansio, "*.xml");
+
+if (tiedostot.Length > 0)
+{
+    foreach (string tiedosto in tiedostot)
+    {
+        // TODO: tähän XML-validiointi
+        string nimi = Path.GetFileName(tiedosto);
+        string kohdePolku = Path.Combine(KäsitellytKansio, nimi);
+        File.Move(tiedosto, kohdePolku);
+    }
+}
