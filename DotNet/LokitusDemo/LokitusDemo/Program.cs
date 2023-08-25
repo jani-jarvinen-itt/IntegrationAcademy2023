12ff866a7fd1160182b413398cfab618c686611c
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-25 10:16:02 +0300

    Lisätty yksinkertainen esimerkki lokituksesta C#-kielllä Log4Net-kirjastoa käyttäen.

---
+++ DotNet/LokitusDemo/LokitusDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using log4net.Config;
+
+ILog log = LogManager.GetLogger(typeof(Program));
+BasicConfigurator.Configure();
+
+log.Info("Testi-ilmoitus.");
+
+try
+{
+	throw new InvalidOperationException("Pöö!");
+}
+catch (Exception ex)
+{
+	log.Error("Odottamaton virhe!", ex);
+}
