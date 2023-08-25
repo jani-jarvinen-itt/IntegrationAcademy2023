4bd18bbf6999c88599b5c8020bfd77eb6a5aa11d
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-25 14:51:13 +0300

    Lisätty C#-kielinen XSLT-muunnosesimerkki.

---
+++ DotNet/XsltMuunnokset/XsltMuunnokset/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Xsl;
+
+Console.WriteLine("XSLT-muunnos käynnistyy.");
+
+// Load the style sheet.
+XslCompiledTransform xslt = new();
+xslt.Load(@"..\..\..\Kirjat.xslt");
+
+// Execute the transform and output the results to a file.
+xslt.Transform(@"..\..\..\Kirjat.xml", "Kirjat-tuloste.html");
+
+Console.WriteLine("XSLT-muunnos valmis.");
