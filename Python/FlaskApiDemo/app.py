03013d00d81817511e66808c0ea24c0d2b1f8ba8
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-28 13:21:08 +0300

    Lisätty Flask-API:ja, jotka käsittelevät parametrejä eri tavoin.

--- Python/FlaskApiDemo/app.py
+++ Python/FlaskApiDemo/app.py
@@ -14,3 +14,12 @@ def luvut():
 def parametrit():
     metodi = request.method
     return f"Tämä pyyntö tehtiin HTTP:n {metodi}-metodilla."
+
+@app.route("/api/asiakas/<int:asiakasid>")
+def hae_asiakas(asiakasid):
+    return f"Tässä asiakkaan {asiakasid} tiedot..."
+
+@app.route("/api/kyselyparametrit")
+def kyselyparametrit():
+    arvo = request.args.get("asiakasid")
+    return f"Asiakas-id:n arvo on: " + str(arvo)
