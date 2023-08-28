c779cdad13e6e97dea1a9c60a8177712e5e082e9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-28 11:42:07 +0300

    Lisätty HTTP API-toiminto joka osaa käsitellä HTTP:n GET- ja POST-pyyntöjä.

--- Python/FlaskApiDemo/app.py
+++ Python/FlaskApiDemo/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Moi maailma Integraatio-koulutusohjelmasta!"
+
+@app.route("/api/luvut")
+def luvut():
+    return [1, 2, 3]
+
+@app.route("/api/parametrit", methods=["GET", "POST", "PUT"])
+def parametrit():
+    metodi = request.method
+    return f"Tämä pyyntö tehtiin HTTP:n {metodi}-metodilla."
