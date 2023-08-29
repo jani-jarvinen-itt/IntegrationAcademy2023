6c617b171db52002d58488bf716b59b7f6c90ad2
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-29 09:45:03 +0300

    Aloitettu C#-sovelluksen koodaus, Web API -demo.

---
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/DemoApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("parametrit")]
+        public string Parametrit(string nimi, int ikä)
+        {
+            return $"Parametrien arvot: nimi={nimi}, ikä={ikä}.";
+        }
+    }
+}
