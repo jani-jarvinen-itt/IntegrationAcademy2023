9787f34516d5c4eff63aa7a805e06b3f202a81ac
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-09-21 10:43:01 +0300

    Lisätty Azure Serverless Function -esimerkki C#:lla.

--- DotNet/AzureFunctionAppDemo/AzureFunctionAppDemo/Function1.cs
+++ DotNet/AzureFunctionAppDemo/AzureFunctionAppDemo/Function1.cs
@@ -39,8 +39,8 @@ namespace AzureFunctionAppDemo
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                ? "Oma testi! This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Moi, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
