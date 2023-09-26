be544b508d4c78208eb73e3cd51e85bdda81ea26
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-09-26 13:55:14 +0300

    Lisätty PowerShell-esimerkki C#-sovelluksen käännösautomaatiosta.

---
+++ PowerShell/DotnetKäännös.ps1
@@ -0,0 +1,21 @@
+﻿# Automaatioskripti .NET-sovelluksen kääntämiseen ja julkaisuun.
+# Tehnyt N.N syyskuussa 2023.
+
+# kansiot
+$lähdekoodit = "C:\Integration\Koodit\DotNet\HelloWorld"
+$kohdekansio = "C:\Install\Integration"
+
+# käännetään .NET-sovellus
+cd $lähdekoodit
+dotnet build
+
+# tarkistetaan onnistuiko käännös (last exit code = 0)
+if ($LASTEXITCODE -eq 0) {
+
+    # luodaan kohdekansiorakenne
+    md -Force $kohdekansio
+
+    # kopioidaan sovelluksen binäärit
+    $binäärit = $lähdekoodit + "\bin\Debug\net7.0\*.*"
+    copy $binäärit -Destination $kohdekansio
+}
