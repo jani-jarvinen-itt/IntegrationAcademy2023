e92b59a7a9e81d941be6ed4da15dc830b73e47d8
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-09-27 14:28:58 +0300

    Lisätty kaksi API-käsittelijää asiakas- ja tilaustiedoille.

---
+++ DotNet/LoppuharjoitusWebApi/NorthwindWebApi/Controllers/AsiakkaatApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthwindWebApi.Controllers
+{
+    [Route("api/asiakas")]
+    [ApiController]
+    public class AsiakkaatApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{asiakasId}")]
+        public string TilausNumerolla(string asiakasId)
+        {
+            return $"Asiakkaan {asiakasId} tiedot..";
+        }
+    }
+}
---
+++ DotNet/LoppuharjoitusWebApi/NorthwindWebApi/Controllers/TilauksetApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthwindWebApi.Controllers
+{
+    [Route("api/tilaus")]
+    [ApiController]
+    public class TilauksetApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{numero:int}")]
+        public string TilausNumerolla(int numero)
+        {
+            return $"Tilauksen {numero} tiedot..";
+        }
+    }
+}
