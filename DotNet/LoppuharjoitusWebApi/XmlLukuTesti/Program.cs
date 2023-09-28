dd219cc84bb1ac31294b1873f91a589c0e8c5952
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-09-28 09:29:29 +0300

    Lisätty XML-aineiston käsittely erilliseen sovellukseen.

--- DotNet/LoppuharjoitusWebApi/XmlLukuTesti/Program.cs
+++ DotNet/LoppuharjoitusWebApi/XmlLukuTesti/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+using System.Xml.Linq;
+
+XDocument dokumentti = XDocument.Load(@"..\..\..\Northwind.xml");
+Console.WriteLine("XML-dokumentti ladattu.");
+
+string igTunnus = HaeAsiakkaanLisätiedot("ALFKI");
+Console.WriteLine(igTunnus);
+
+string lisätiedot = HaeTilauksenLisätiedot(10248);
+Console.WriteLine(lisätiedot);
+
+string HaeAsiakkaanLisätiedot(string asikasId)
+{
+    var asiakasElementit = dokumentti.Descendants("Customer");
+
+    XElement? asiakas = asiakasElementit.FirstOrDefault(
+        c => c.Element("CustomerID")?.Value == asikasId);
+    if (asiakas != null)
+    {
+        string igTunnus = asiakas.Element("SocialMedia")?.Element("Instagram")?.Value ?? "";
+        return igTunnus;
+    }
+    else
+    {
+        return "";
+    }
+}
+
+string HaeTilauksenLisätiedot(int tilausNro)
+{
+    var tilausElementit = dokumentti.Descendants("Order");
+
+    XElement? tilaus = tilausElementit.FirstOrDefault(
+        c => int.Parse(c.Element("OrderID")?.Value ?? "0") == tilausNro);
+    if (tilaus != null)
+    {
+        string lisätiedot = tilaus.Element("SpecialInstructions")?.Value ?? "";
+        return lisätiedot;
+    }
+    else
+    {
+        return "";
+    }
+}
